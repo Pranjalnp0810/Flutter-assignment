4a2974fdf5a49d236ddbeb126d709ec023adafd2
Pranjalnp0810 <70252691+Pranjalnp0810@users.noreply.github.com>
2024-02-06 15:27:59 +0530

    Assessment1
    Offline database

---
+++ Assessment1/lib/productDisplay.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:inventorymanagementapp/Database/Model/product.dart';
+import 'package:inventorymanagementapp/Database/db.dart';
+
+class MyProductDisplay extends StatefulWidget {
+  const MyProductDisplay({super.key});
+
+  @override
+  State<MyProductDisplay> createState() => _MyProductDisplayState();
+}
+
+class _MyProductDisplayState extends State<MyProductDisplay> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getProductDisplay();
+  }
+
+  List<ProductModel> productList = <ProductModel>[];
+  var productDp = DatabaseConnectionA();
+
+  getProductDisplay() async {
+    var productD = await productDp.displayProduct();
+    productD.forEach((rawData) {
+      setState(() {
+        var productModel = ProductModel();
+        productModel.id = rawData['id'];
+        productModel.name = rawData['name'];
+        productModel.category = rawData['category'];
+        productModel.price = rawData['price'];
+        productModel.number = rawData['number'];
+
+        productList.add(productModel);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.builder(
+        itemCount: productList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(
+                "${productList[index].name}",
+                style: TextStyle(fontSize: 30),
+              ),
+              subtitle: Text(
+                "\$ ${productList[index].price}",
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Name : ${productList[index].name}",
+                                style: TextStyle(fontSize: 50),
+                              ),
+                              Divider(
+                                height: 2,
+                                thickness: 2,
+                              ),
+                              SizedBox(
+                                height: 19,
+                              ),
+                              Text(
+                                "Category : ${productList[index].category}",
+                                style: TextStyle(fontSize: 37),
+                              ),
+                              Divider(
+                                height: 2,
+                                thickness: 1,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    "Price :\$ ${productList[index].price}",
+                                    style: TextStyle(fontSize: 27),
+                                  ),
+                                ],
+
+                              ),
+                              SizedBox(
+                                height: 140,
+                              ),
+                               Text(
+                                    "Number : ${productList[index].number}",
+                                    style: TextStyle(fontSize: 27),
+                                  ),
+                            ]),
+                      ),
+                    );
+                  },
+                );
+              
+              
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
